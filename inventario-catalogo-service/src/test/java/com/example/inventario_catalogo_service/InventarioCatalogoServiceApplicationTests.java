859f82a567470bcf3ec3f59248fdb67331566039
nicoGit <ni.quezadah@duocuc.cl>
2026-05-19 20:13:51 -0400

    estructura inicial de microservicios (inventario-catalogo, soporte-resena, tiendas)

---
+++ inventario-catalogo-service/src/test/java/com/example/inventario_catalogo_service/InventarioCatalogoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inventario_catalogo_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioCatalogoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ tiendas-service/src/test/java/com/example/tiendas_service/TiendasServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tiendas_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendasServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
